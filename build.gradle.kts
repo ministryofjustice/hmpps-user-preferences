25b6e873127b52e738e25bb459d075df294611cd
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2021-01-29 07:59:04 +0000

    :wrench: add postgres db for local dev and test environment

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,5 +8,11 @@ configurations {
 }
 
 dependencies {
+  // security
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+  // database
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.springframework.boot:spring-boot-starter-validation")
+  runtimeOnly("org.postgresql:postgresql")
 }
