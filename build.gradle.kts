157d8e5533ed0298f95bce3907c14e4d257444ef
theDustRoom <paul.massey@digital.justice.gov.uk>
2021-02-03 14:58:07 +0000

    :wrench: Configure Flyway
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.1"
   kotlin("plugin.spring") version "1.4.10"
-  id("org.jetbrains.kotlin.plugin.jpa") version "1.4.20"
+  id("org.jetbrains.kotlin.plugin.jpa") version "1.4.21"
 }
 
 configurations {
@@ -18,5 +18,6 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-validation")
   implementation("org.hibernate:hibernate-core:5.4.24.Final")
   implementation("com.vladmihalcea:hibernate-types-52:2.10.2")
+  runtimeOnly("org.flywaydb:flyway-core")
   runtimeOnly("org.postgresql:postgresql")
 }
