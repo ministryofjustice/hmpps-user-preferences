aceb48236e930dcfc4293085f21c5cd47c23c006
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2021-02-12 08:17:11 +0000

    :sparkles: PIC-1076: Added jwt decoder to test profile

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsuserpreferences/config/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsuserpreferences/config/ResourceServerConfiguration.kt
@@ -1,4 +1,5 @@
 package uk.gov.justice.digital.hmpps.hmppsuserpreferences.config
+import com.nimbusds.jwt.JWTParser
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
@@ -6,24 +7,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.MappedJwtClaimSetConverter
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter
 
 @Configuration
 @EnableWebSecurity
-@Profile("test")
-class BasicResourceServerConfiguration : WebSecurityConfigurerAdapter() {
-  override fun configure(http: HttpSecurity) {
-    http
-      .sessionManagement()
-      .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-      .and().csrf().disable()
-  }
-}
-
-@Configuration
-@EnableWebSecurity
-@Profile("!test")
 class ResourceServerConfiguration : WebSecurityConfigurerAdapter() {
   override fun configure(http: HttpSecurity) {
     http
@@ -31,7 +22,7 @@ class ResourceServerConfiguration : WebSecurityConfigurerAdapter() {
       .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
       .and().csrf().disable()
       .authorizeRequests {
-        it.antMatchers("/health/**", "/test/**", "/info").permitAll()
+        it.antMatchers("/health/**", "/info").permitAll()
         it.anyRequest().authenticated()
       }
       .oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter())
@@ -48,5 +39,27 @@ class ResourceServerConfiguration : WebSecurityConfigurerAdapter() {
     val jwtAuthenticationConverter = JwtAuthenticationConverter()
     jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter)
     return jwtAuthenticationConverter
+  }
+
+  @Bean
+  @Profile("test")
+  fun testJwtDecoder(): JwtDecoder {
+    return TestJwtDecoder()
+  }
+}
+
+internal class TestJwtDecoder : JwtDecoder {
+  private val claimSetConverter = MappedJwtClaimSetConverter.withDefaults(emptyMap())
+
+  override fun decode(token: String): Jwt? {
+    // extract headers and claims, but do not attempt to verify signature
+    val jwt = JWTParser.parse(token)
+    val headers = LinkedHashMap<String, Any>(jwt.header.toJSONObject())
+    val claims = claimSetConverter.convert(jwt.getJWTClaimsSet().claims)
+
+    return Jwt.withTokenValue(token)
+      .headers { it.putAll(headers) }
+      .claims { it.putAll(claims) }
+      .build()
   }
 }
