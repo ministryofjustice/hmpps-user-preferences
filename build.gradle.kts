c29e4638a91dad2c13a389d4cd478bab55965b35
John Evans <john.evans@digital.justice.gov.uk>
2022-04-01 09:28:44 +0100

    🔥 PIC-1446: Remove redundant dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,6 @@ dependencies {
   // database
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-validation")
-  implementation("org.hibernate:hibernate-core:5.6.7.Final")
   implementation("com.vladmihalcea:hibernate-types-52:2.14.1")
   runtimeOnly("org.flywaydb:flyway-core")
   runtimeOnly("org.postgresql:postgresql")
