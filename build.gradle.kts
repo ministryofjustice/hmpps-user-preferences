d4f91096a0f3986353ef8b01a66f069e1608c09a
John Evans <john.evans@digital.justice.gov.uk>
2022-05-26 11:50:53 +0100

    🔥 PIC-1446: Remove redundant hibernate dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,8 +39,6 @@ dependencies {
   // database
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-validation")
-  implementation("org.hibernate:hibernate-core:5.6.7.Final")
-  implementation("com.vladmihalcea:hibernate-types-52:2.16.2")
   runtimeOnly("org.flywaydb:flyway-core")
   runtimeOnly("org.postgresql:postgresql")
   testRuntimeOnly("com.h2database:h2:1.4.200")
