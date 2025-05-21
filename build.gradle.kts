7c173da0dba4e51a7912fd01ee45003c7167b477
lmorison <lisa.morison@justice.gov.uk>
2025-05-21 14:19:41 +0100

    Pic 4656/fix security vulnerabilities (#418)
    * update deployment with securityContext
    
    * remove fsGroup from securityContext
    
    * remove fsGroup from securityContext to fix pipeline
    
    * upgrade dockerfile build image
    
    * revert to jdk slim buster
    
    * revert to jdk slim buster
    
    * update securitycontext in deployment
    
    * update security context to fix pipeline failure
    
    * remove ai logger from logback
    
    * add ai logging dependency
    
    * upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
   kotlin("plugin.spring") version "2.0.20"
   kotlin("jvm") version "2.0.20"
   id("org.jetbrains.kotlin.plugin.jpa") version "1.8.0"
-  id("org.flywaydb.flyway") version "9.14.0"
+  id("org.flywaydb.flyway") version "11.3.4"
 }
 val pactVersion = "4.3.16"
 
@@ -48,12 +48,14 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-validation")
   runtimeOnly("org.flywaydb:flyway-core")
+  runtimeOnly("org.flywaydb:flyway-database-postgresql:11.3.4")
   runtimeOnly("org.postgresql:postgresql:42.5.5")
   testRuntimeOnly("com.h2database:h2:1.4.200")
 
   implementation("org.springdoc:springdoc-openapi-ui:1.6.14")
   implementation("jakarta.xml.bind:jakarta.xml.bind-api:3.0.1")
   implementation("com.microsoft.azure:applicationinsights-web:3.5.4")
+  implementation("com.microsoft.azure:applicationinsights-logging-logback:2.6.4")
   implementation("org.apache.commons:commons-lang3:3.14.0")
 
   // Test
