96350606c23fc8830b2c65c622b67d4bd3d33fe5
lmorison <lisa.morison@justice.gov.uk>
2025-07-21 13:45:23 +0100

    fix security vulnerabilities (#422)
    * fix security vulnerabilities
    
    * upgrade kotlin plugins and scan pr
    
    * remove temp pr scans

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.2.0"
-  kotlin("plugin.spring") version "2.1.21"
-  kotlin("jvm") version "2.1.21"
-  kotlin("plugin.jpa") version "2.1.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.2"
+  kotlin("plugin.spring") version "2.2.0"
+  kotlin("jvm") version "2.2.0"
+  kotlin("plugin.jpa") version "2.2.0"
   id("org.flywaydb.flyway") version "11.3.4"
 }
 val pactVersion = "4.3.16"
