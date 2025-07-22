e3825256620f2e2706546663de6feb0d8588d7e3
lmorison <lisa.morison@justice.gov.uk>
2025-07-22 15:15:13 +0100

    Pic 4754/fix failing security scans (#423)
    * fix security vulnerabilities
    
    * upgrade kotlin plugins and scan pr
    
    * remove temp pr scans
    
    * upgrade hmpps spring boot version to fix security scan failure

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.3"
   kotlin("plugin.spring") version "2.2.0"
   kotlin("jvm") version "2.2.0"
   kotlin("plugin.jpa") version "2.2.0"
